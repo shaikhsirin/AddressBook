aadca905f8b5144dea042471f8ec33ecd87917f3
shaikhsirin <shaikhsirin8080example.com>
2022-03-16 00:18:04 +0530

    Welcome to Address Book

---
+++ src/AddressBookMain.java
@@ -0,0 +1,141 @@
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddressBookMain {
+    ArrayList<Contact> Book = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+
+    public void addDetails() {
+
+        Scanner scanner = new Scanner(System.in);
+        Contact details = new Contact();
+        System.out.println("Enter the first name of person");
+        details.setFirstName(scanner.nextLine());
+        System.out.println("Enter the last name of person");
+        details.setLastName(scanner.nextLine());
+        System.out.println("Enter the Address Name of person");
+        details.setAddress(scanner.nextLine());
+        System.out.println("Enter the city Name of person");
+        details.setCity(scanner.nextLine());
+        System.out.println("Enter the State Name of person");
+        details.setState(scanner.nextLine());
+        System.out.println("Enter the Email of person");
+        details.setEmail(scanner.nextLine());
+        System.out.println("Enter the zipcode of person");
+        details.setZipCode(scanner.nextInt());
+        System.out.println("Enter the phone number of person");
+        details.setPhoneNumber(scanner.nextInt());
+
+        Book.add(details);
+
+        System.out.println(Book);
+    }
+
+    /*
+     *purpose : used edit method to edit the details
+     */
+    public void editDetails() {
+
+        System.out.println("Confirm your first name to edit details: ");
+        String name = scanner.next();
+
+        for (int i = 0; i < Book.size(); i++) {
+            if (Book.get(i).getFirstName().equals(name)) {
+                System.out.println("select form below to change: ");
+                System.out.println("\n1.First Name\n2.LastName\n3.Address\n4.city\n5.state\n6.Email\n7.zipCode\n8.phoneNumber");
+                int check = scanner.nextInt();
+
+                switch (check) {
+                    case 1:
+                        System.out.println("Enter first name");
+                        Book.get(i).setFirstName(scanner.next());
+                        break;
+                    case 2:
+                        System.out.println("Enter last name");
+                        Book.get(i).setLastName(scanner.next());
+                        break;
+                    case 3:
+                        System.out.println("Enter address");
+                        Book.get(i).setAddress(scanner.next());
+                        break;
+                    case 4:
+                        System.out.println("Enter city");
+                        Book.get(i).setCity(scanner.next());
+                        break;
+                    case 5:
+                        System.out.println("Enter state");
+                        Book.get(i).setState(scanner.next());
+                        break;
+                    case 6:
+                        System.out.println("Enter email");
+                        Book.get(i).setEmail(scanner.next());
+                        break;
+                    case 7:
+                        System.out.println("Enter zip");
+                        Book.get(i).setZipCode(scanner.nextInt());
+                        break;
+                    case 8:
+                        System.out.println("Enter phone number");
+                        Book.get(i).setPhoneNumber(scanner.nextInt());
+                        break;
+                }
+
+                System.out.println(Book);
+            } else
+                System.out.println("Enter valid first name");
+        }
+    }
+
+    /*
+     *purpose : used output method to display the details
+     */
+    public void output() {
+        //TODO Auto-generated method stub
+        System.out.println(Book);
+    }
+
+    /*
+     *purpose : used deletDetails method of the person
+     */
+    public void deleteDetails() {
+        System.out.println("Confirm your first name to edit details: ");
+        String name = scanner.next();
+
+        for (int i = 0; i < Book.size(); i++) {
+            if(Book.get(i).getFirstName().equals(name)){
+            System.out.println("Select form below to change: ");
+            Book.remove(i);
+        }
+    }
+        System.out.println(Book);
+
+}
+public static void main(String[] args) {
+        int i = 0;
+        System.out.println("Welcome to adress management sysytem");
+        AddressBookMain details = new AddressBookMain();
+
+        while (i == 0) {
+            System.out.println("what you want do: ");
+            System.out.println("delete details");
+            int choose = scanner.nextInt();
+            switch (choose) {
+                case 1:
+                    details.addDetails();
+                    break;
+                case 2:
+                    details.editDetails();
+                    break;
+                case 3:
+                    details.deleteDetails();
+                    break;
+                default:
+                    i = 1;
+                    System.out.println("wrong option");
+                    details.output();
+                    break;
+            }
+        }
+}
+}
